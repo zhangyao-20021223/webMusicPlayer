d4ca19aa5b1d5abbaa303fc7bac8e6b33cba4284
张耀 <55308090+zhangyao-20021223@users.noreply.github.com>
2022-02-06 16:49:58 +0800

    提交

--- js/player.js
+++ js/player.js
@@ -1,12 +1,12 @@
 var xhr = new XMLHttpRequest;
 // xhr.open("GET", "https://zk-nhbook.osdn.io/getMusic.php");
-xhr.open("GET", "json.json");
+xhr.open("GET", "../json.json");
 xhr.responseType = "text";
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
         if ((xhr.status >= 200 && xhr.status < 300) || (xhr.status == 304)) {
-            var result = JSON.parse(xhr.responseText);
-            setUpFplayer(result);
+            result = JSON.parse(xhr.responseText);
+
         } else {
             alert("播放器加载失败");
         }
@@ -14,28 +14,7 @@ xhr.onreadystatechange = function() {
 }
 xhr.send();
 
-function setUpFplayer(result) {}
-
-
-var FPlayer_Button_js = document.getElementById("musicPlayerControlBar");
-for (let a = 0; a < 4; a++) {
-    FPlayer_Button_js.getElementsByTagName("img")[a].addEventListener('click', function() {
-        switch (this.id) {
-            case "button_qianjin":
-                break;
-            case "button_zanting":
-                break;
-            case "button_houtui":
-                break;
-            case "button_liebiao":
-                break;
-        }
-    })
-}
-
-//FPlayer的核心
-
-
-function FPlayer_Audio_core() {
-
-}
+var FPlayer = {}
+FPlayer.zanting = function() {}
+FPlayer.qianjin = function() {}
+FPlayer.houtui = function() {}
--- js/style.js
+++ js/style.js
@@ -1,40 +1,47 @@
 var FPlayer_bar_js = document.getElementById("musicPlayerBeforeBar"); //FPlayer整体的js
-var button_liebiao_js = document.getElementById("button_liebiao"); //列表的按钮的js
 var musicPlayerAllBar_js = document.getElementById("musicPlayerAllBar"); //控制面板的js
 var musicPlayerLyricBar_js = document.getElementById("musicPlayerLyricBar"); //歌词按钮的js && 歌词整体的js
-var liebiao_js = document.getElementById("liebiao"); //列表整体的js
-//交互上的逻辑为：
-//       列表面板：
-//              列表按钮触发列表到最顶层，期间涉及到z-index的调整，如果对页面有影响可以自行更改
-//              点击列表中的任意一首歌曲都会触发音乐填充并且直接播放，长按会触发保存歌曲图片/复制文字等操作，并触发已保存/已复制的文字
-//       歌词面板：
-//              直接点击歌词触发歌词条变大直到填充整个播放器，点击任意歌词面板空白处可以收缩歌词面板
-//              直接点击歌词会触发复制，并且弹出“已复制”的图标
-//              上下滑动歌词可以调整进度，目前只有这一种调整进度的方式后续应该会更新（随缘
-//       控制面板：
-//              控制面板初始化在最上层，有四个显式按钮，三个隐式触发事件，四个按钮分别是（从左到右顺序）上一首/暂停/下一首/打开列表
-//              三个隐式按钮是：歌曲名称显式块/作曲家显式块/歌曲图片显式块，三个块长按可以触发复制/复制/保存图片，功能
-//              
-//              
-//              
-//              
-//              
-//
+var musicPlayerListBar_js = document.getElementById("musicPlayerListBar"); //列表整体的js
+var musicPlayerLyricBar_gundong_div = document.getElementById("musicPlayerLyricBar_gundong_div"); //歌词显示面板的js
+var button_liebiao_js = document.getElementsByClassName("button_liebiao"); //列表的按钮的js
+var huandong = "transition: all 600ms cubic-bezier(.23, 1, .32, 1);animation-duration: 5s;"
+    //交互上的逻辑为：
+    //       列表面板：
+    //              列表按钮触发列表到最顶层，期间涉及到z-index的调整，如果对页面有影响可以自行更改
+    //              点击列表中的任意一首歌曲都会触发音乐填充并且直接播放，长按会触发保存歌曲图片/复制文字等操作，并触发已保存/已复制的文字
+    //       歌词面板：
+    //              直接点击歌词触发歌词条变大直到填充整个播放器，点击任意歌词面板空白处可以收缩歌词面板
+    //              直接点击歌词会触发复制，并且弹出“已复制”的图标
+    //              上下滑动歌词可以调整进度，目前只有这一种调整进度的方式后续应该会更新（随缘
+    //       控制面板：
+    //              控制面板初始化在最上层，有四个显式按钮，三个隐式触发事件，四个按钮分别是（从左到右顺序）上一首/暂停/下一首/打开列表
+    //              三个隐式按钮是：歌曲名称显式块/作曲家显式块/歌曲图片显式块，三个块长按可以触发复制/复制/保存图片，功能
 
-var FPlayer_bar_js_height = FPlayer_bar_js.offsetHeight;
 
-var FPlayer_bar_js_width = FPlayer_bar_js.offsetWidth;
+var musicPlayerLyricBarFlag = false;
+musicPlayerLyricBar_gundong_div.addEventListener("click", function() {
+    if (musicPlayerLyricBarFlag == false) {
+        musicPlayerLyricBar_js.style.cssText = "width: 100%;height:100%";
+        this.style.cssText = "width:100%;height:100%;overflow: scroll;" + huandong;
+        musicPlayerLyricBarFlag = true;
+    } else if (musicPlayerLyricBarFlag == true) {
+        musicPlayerLyricBar_js.style.cssText = "width: 100%;height:27%";
+        this.style.cssText = "width:90%;height:80%;margin-left:5%;" + huandong;
+        musicPlayerLyricBarFlag = false;
+    }
+})
 
-button_liebiao_js.onclick = function button_liebiao_js_onclick() {
+var musicPlayerListBarFlag = false;
+button_liebiao_js[0].addEventListener('click', function() {
+    if (musicPlayerListBarFlag == false) {
+        musicPlayerListBar_js.style.cssText = "z-index: 1;transform: translateY(0px);" + huandong;
+        musicPlayerListBarFlag = true;
+    }
 
-    musicPlayerLyricBar_js.style.height = FPlayer_bar_js_height + "px";
-
-    musicPlayerLyricBar_js.style.width = FPlayer_bar_js_width + "px";
-
-    musicPlayerLyricBar_js.style.color = "#000";
-
-    musicPlayerLyricBar_js.style.zIndex = "2";
-
-    musicPlayerLyricBar_js.style.opacity = "1";
-
-}
+})
+musicPlayerListBar_js.addEventListener('click', function() {
+    if (musicPlayerListBarFlag == true) {
+        musicPlayerListBar_js.style.cssText = "z-index:1;transform: translateY(160px);" + huandong
+        musicPlayerListBarFlag = false;
+    }
+})
